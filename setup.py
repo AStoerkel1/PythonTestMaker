7740c423966552625c5356711fc5658d4648fb87
Andrew <astoer1@students.towson.edu>
2022-04-05 17:30:59 -0400

    fixed version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="PythTest",
-    version="2.0.0",
+    version="0.0.1",
     description="Unit Test Builder for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
