afc083a577b85a0f49d4a1f41a2a641f8a588c30
Andrew <astoer1@students.towson.edu>
2022-04-10 14:05:33 -0400

    created a better way of retrieving and writing function signatures for tests

--- PythTest/src/createTests.py
+++ PythTest/src/createTests.py
@@ -1,21 +1,18 @@
 
-def writeSig(line: str, testFile):
-    """
-    isolates the function signature and 
-    writes it to a new file
+from inspect import getmembers, isfunction, isclass
+import sys
 
-    TO-DO: eventually I will write a runner which will call
-    all of the tests instead of calling them from 
-    inside the test file
-    
+def writeSig(funList: list, testFile):
+    """takes a dictionary of classes and functions
+    and writes the test function signatures for them
+
     Args:
-        line (str): line with a function signature in it
+        functionDict (list): list of functions for a class or module
+        testFile (file object): the test file to write to
     """
-    signature = line.split()[-1].capitalize()
-    endOfName = signature.find('(')
-    name = signature[:endOfName]
-    testSig = f'def test{name}():'
-    testFile.write(f'@test\n{testSig}\n\tassert False, "test not implemented"\ntest{name}()')
+    for name in funList:
+        testSig = f'def test{name}():'
+        testFile.write(f'@test\n{testSig}\n\tassert False, "test not implemented"\ntest{name}()\n')
 
     
 
@@ -42,8 +39,26 @@ def createTestFile(fileUnderTest: str):
     with open(testFile, 'w') as testF:
         testF.write('from PythTest import *\n')
         testF.write(f'from {fileUnderTest[:-3]} import *\n')
-        for l in lines: 
-            if l.strip().startswith('def'):
-                writeSig(l, testF)           
+        funDict = getFun(fileUnderTest[:-3])
+        for val in funDict.values():
+            writeSig(val, testF)
 
+def getFun(mod):
+    """make a dictionary where the key is the class name and the 
+        values are a list of it's function names
+        dict = {'className':['fun1','fun2', ...], ... : [...]}
 
+        Args:
+            mod(str): the module to test
+    """
+    __import__(mod)
+    myMod = sys.modules[mod]
+    #holds a list of classes as [('className', <object reference>), ...]
+    #this line will pick out the members of the imported module and if it is a class add it to the list
+    classes = [o for o in getmembers(myMod) if isclass(o[1])]
+
+    #this will build a dictionary where the key is the class name and the value is a list of that class's functions
+    #Warning: do not meddle with forces you do not fully understand
+    newDict={key:val for (key, val) in zip([c[0] for c in classes],[[f[0] for f in getmembers(c[1]) if isfunction(f[1])] for c in classes])}
+
+    return(newDict)
--- factorial.py
+++ factorial.py
@@ -8,4 +8,14 @@ class Factorial():
             ans*=n
             n-=1
         return ans
+    
+    def factFun2():
+        return 1
 
+class x():
+    def xfun():
+        return True
+
+class y():
+    def yfun():
+        return False
