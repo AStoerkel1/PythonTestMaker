3925a5bd9a5fc2ab0bf18807fbbbbfb1ce4d7569
Andrew <astoer1@students.towson.edu>
2022-04-10 15:13:26 -0400

    modified getFunctions to get functions not associated with classes

--- PythTest/src/createTests.py
+++ PythTest/src/createTests.py
@@ -33,13 +33,14 @@ def createTestFile(fileUnderTest: str):
     with open(testFile, 'w') as testF:
         testF.write('from PythTest import *\n')
         testF.write(f'from {fileUnderTest[:-3]} import *\n')
-        funDict = getFun(fileUnderTest[:-3])
+        funDict = getFunctions(fileUnderTest[:-3])
         for val in funDict.values():
             writeStub(val, testF)
 
-def getFun(mod):
+def getFunctions(mod):
     """make a dictionary where the key is the class name and the 
-        values are a list of it's function names
+        values are a list of it's function names also includes
+        functions which aren't members of classes
         dict = {'className':['fun1','fun2', ...], ... : [...]}
 
         Args:
@@ -47,6 +48,10 @@ def getFun(mod):
     """
     __import__(mod)
     myMod = sys.modules[mod]
+
+    #gets the functions not in a class from the given file
+    freeFunctions = [f for f in getmembers(myMod) if isfunction(f[1])]
+
     #holds a list of classes as [('className', <object reference>), ...]
     #this line will pick out the members of the imported module and if it is a class add it to the list
     classes = [o for o in getmembers(myMod) if isclass(o[1])]
@@ -54,5 +59,6 @@ def getFun(mod):
     #this will build a dictionary where the key is the class name and the value is a list of that class's functions
     #Warning: do not meddle with forces you do not fully understand
     newDict={key:val for (key, val) in zip([c[0] for c in classes],[[f[0] for f in getmembers(c[1]) if isfunction(f[1])] for c in classes])}
-
+    newDict.update({'no Class': [n[0] for n in functions]})
+    
     return(newDict)
--- factorial.py
+++ factorial.py
@@ -1,16 +1,16 @@
 #simple function to find a factorial
-class Factorial():
+
     
-    def n_factorial():
-        n = 5
-        ans=1
-        while n >0:
-            ans*=n
-            n-=1
-        return ans
-    
-    def factFun2():
-        return 1
+def n_factorial():
+    n = 5
+    ans=1
+    while n >0:
+        ans*=n
+        n-=1
+    return ans
+
+def factFun2():
+    return 1
 
 class x():
     def xfun():
