74d11687de4541152614229b059c180c5909abec
Andrew <astoer1@students.towson.edu>
2022-04-10 14:14:50 -0400

    minor changes for comments and function names

--- PythTest/src/createTests.py
+++ PythTest/src/createTests.py
@@ -2,15 +2,16 @@
 from inspect import getmembers, isfunction, isclass
 import sys
 
-def writeSig(funList: list, testFile):
+def writeStub(funList: list, testFile):
     """takes a dictionary of classes and functions
     and writes the test function signatures for them
 
     Args:
-        functionDict (list): list of functions for a class or module
+        funList (list): list of functions for a class or module
         testFile (file object): the test file to write to
     """
     for name in funList:
+        name = name.capitalize()
         testSig = f'def test{name}():'
         testFile.write(f'@test\n{testSig}\n\tassert False, "test not implemented"\ntest{name}()\n')
 
@@ -18,30 +19,23 @@ def writeSig(funList: list, testFile):
 
 def createTestFile(fileUnderTest: str):
     testFile = f'test{fileUnderTest.capitalize()}'
-    
-    """
-    open the file and readlines() as lines
-    with does not create scope
-    """
-    with open(fileUnderTest, 'r') as file:
-        lines = file.readlines()
 
     """
     open the testFile and overwrite if
     it already exists
-    write import for wrappers
-    look for function signatures
-    call writeSig() if one is found
+    write import for file under test
+    call getFun to get functions
+    call writeSig()
 
     Args:
-        lines (list): list of all the lines
+        fileUnderTest (str): name of the module under test
     """
     with open(testFile, 'w') as testF:
         testF.write('from PythTest import *\n')
         testF.write(f'from {fileUnderTest[:-3]} import *\n')
         funDict = getFun(fileUnderTest[:-3])
         for val in funDict.values():
-            writeSig(val, testF)
+            writeStub(val, testF)
 
 def getFun(mod):
     """make a dictionary where the key is the class name and the 
@@ -49,7 +43,7 @@ def getFun(mod):
         dict = {'className':['fun1','fun2', ...], ... : [...]}
 
         Args:
-            mod(str): the module to test
+            mod(str): the name of the module under test
     """
     __import__(mod)
     myMod = sys.modules[mod]
